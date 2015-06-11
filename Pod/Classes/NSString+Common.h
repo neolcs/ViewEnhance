b81a641a26e7c73f4c06399aa67eecc91e128606
Chuan <cli@tradeshift.com>
2015-06-11 15:29:11 +0800

    Add NSString Category

---
+++ Pod/Classes/NSString+Common.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Common.h
+//  Pods
+//
+//  Created by Chuan Li on 6/11/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Common)
+
+@end
---
+++ Pod/Classes/NSString+Common.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Common.m
+//  Pods
+//
+//  Created by Chuan Li on 6/11/15.
+//
+//
+
+#import "NSString+Common.h"
+
+@implementation NSString (Common)
+
+@end
